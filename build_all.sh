4020a3b3e5acfade06a12cda43bf2b8c8f840dd9
Marcus <marcus.mellor@eagles.oc.edu>
2022-02-17 21:35:04 -0600

    [WIP] Updated roct-thunk-interface to 5.0.1
    Changed roct-thunk-interface to version 5.0.1. Added pkgconfig for
    libdrm to complete build. See comments in build_all.sh for info on
    progress.

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #
 
+# Build Order:
+# 1. [OK]   numactl
+# 2. [OK]   ROCT-Thunk-Interface
+# 3. [WIP]  ROCR-Runtime
+# 4. [WIP]  LLVM (roc version)
+# 5. [WIP]  rocm-cmake
+# 6. [WIP]  ROCm-CompilerSupport
+# 7. [WIP]  ROCm-Device-Libs
+# 8. [WIP]  ROCM-OpenCL-Runtime
+# 9. [WIP]  rocminfo
+
 f_build()
 {
     make local
@@ -15,22 +26,22 @@ cd ../roct-thunk-interface/
 f_build
 
 cd ../rocr-runtime
-f_build
+#f_build
 
 cd ../llvm-roc
-f_build
+#f_build
 
 cd ../rocm-cmake
-f_build
+#f_build
 
 cd ../rocm-comgr
-f_build
+#f_build
 
 cd ../rocm-device-libs
-f_build
+#f_build
 
 cd ../rocm-opencl-runtime
-f_build
+#f_build
 
 cd ../rocminfo
-f_build
+#f_build
