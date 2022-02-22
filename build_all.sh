bbae6ad49fa8948fdff1541f7428243bc9c3e69c
Marcus <marcus.mellor@eagles.oc.edu>
2022-02-21 18:18:38 -0600

    [WIP] Initial work on llvm-roc
    Brings llvm-roc up to v5.0.1 and attempt to build. Currently fails
    during the install step. This seems initally to be a permissions
    issue. Further investigation required.

--- build_all.sh
+++ build_all.sh
@@ -4,8 +4,16 @@
 # Build Order:
 # 1. [OK]   numactl
 # 2. [OK]   ROCT-Thunk-Interface
-# 3. [WIP]  ROCR-Runtime
-# 4. [WIP]  LLVM (roc version)
+# 3. [FAIL] LLVM (roc version)
+#   CMake Error at tools/lld/cmake/modules/cmake_install.cmake:58 (file):
+#     file cannot create directory:
+#     /home/build/YPKG/root/llvm-roc/install/usr/lib64/llvm/roc/lib/cmake/lld.
+#     Maybe need administrative privileges.
+#   Call Stack (most recent call first):
+#     tools/lld/cmake_install.cmake:62 (include)
+#     tools/cmake_install.cmake:54 (include)
+#     cmake_install.cmake:77 (include)
+# 4. [WIP]  ROCr-Runtime
 # 5. [WIP]  rocm-cmake
 # 6. [WIP]  ROCm-CompilerSupport
 # 7. [WIP]  ROCm-Device-Libs
@@ -25,10 +33,10 @@ sudo cp *.eopkg /var/lib/solbuild/local/
 cd ../roct-thunk-interface/
 f_build
 
-cd ../rocr-runtime
-#f_build
-
 cd ../llvm-roc
+f_build
+
+cd ../rocr-runtime
 #f_build
 
 cd ../rocm-cmake
